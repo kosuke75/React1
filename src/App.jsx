1c8c105305a48558214006432ab8e7527f63b66e
kosuke75 <kousuke_2527.1@icloud.com>
2024-11-10 23:18:31 +0900

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,18 @@
-
+import React from 'react';
 import Header from './components/Header';
-import Image from './components/Image';
 import Navigate from './components/Navigate';
-import Welcome from './components/Welcome';
 import Main from './components/Main';
+import Welcome from './components/Welcome';
+import Image from './components/Image';
 import Footer from './components/Footer';
 
 export default function App() {
   return (
     <div className="App">
       <Header />
-      <Navigate />
-      <Welcome />
       <Main />
-      <Image />
       <Footer />
     </div>
   );
 }
+
